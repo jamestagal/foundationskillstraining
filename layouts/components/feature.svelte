f96621b59b02fdc18d7c21bd1dbd4fdc8a574908
jamestagal <benjaminjameswaller@gmail.com>
2023-09-22 16:42:38 +1000

    update feature card content

--- layouts/components/feature.svelte
+++ layouts/components/feature.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let title1, title2, subtitle, card1, card2, card3;
+    export let title1, title2, subtitle, card1, card2, card3, card4, card5, card6;
 </script>
 <!-- feature area -->
 <div class="feature-area py-120">
@@ -43,6 +43,39 @@
                     <div class="feature-content">
                         <h4>{card3.title}</h4>
                         <p>{card3.body}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="feature-item">
+                    <div class="feature-icon feature-icon-1">
+                        <i class="far fa-book-open"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h4>{card4.title}</h4>
+                        <p>{card4.body}.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="feature-item active">
+                    <div class="feature-icon feature-icon-2">
+                        <i class="far fa-users"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h4>{card5.title}</h4>
+                        <p>{card5.body}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="feature-item">
+                    <div class="feature-icon feature-icon-3">
+                        <i class="far fa-clock"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h4>{card6.title}</h4>
+                        <p>{card6.body}</p>
                     </div>
                 </div>
             </div>
