111682d509a75f728e770ca1f3fde63d001d3601
jamestagal <benjaminjameswaller@gmail.com>
2023-09-29 19:16:31 +1000

    remove unncessary nav items

--- layouts/global/header.svelte
+++ layouts/global/header.svelte
@@ -25,234 +25,24 @@
             <span class="navbar-toggler-icon"><i class="far fa-bars" /></span>
           </button>
         </div>
-        <div class="nav-category">
-          <div class="dropdown">
-            <button
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <i class="fas fa-grip-vertical" /> Category
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href=".">Software Development</a>
-              </li>
-              <li><a class="dropdown-item" href=".">Web Development</a></li>
-              <li><a class="dropdown-item" href=".">Graphics Design</a></li>
-              <li><a class="dropdown-item" href=".">Motion Graphics</a></li>
-              <li><a class="dropdown-item" href=".">Digital Marketing</a></li>
-              <li><a class="dropdown-item" href=".">Video Edition</a></li>
-              <li><a class="dropdown-item" href=".">Logo Design</a></li>
-              <li><a class="dropdown-item" href=".">English Learning</a></li>
-            </ul>
-          </div>
-        </div>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle active"
-                href="."
-                data-bs-toggle="dropdown">Home</a
-              >
-              <ul class="dropdown-menu fade-up">
-                <li>
-                  <a class="dropdown-item" href="index.html">Home Page 01</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="index-2.html">Home Page 02</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="index-3.html">Home Page 03</a>
-                </li>
-              </ul>
+            <li class="nav-item">
+              <a class="nav-link" href=".">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="aboutus">About Us</a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="."
-                data-bs-toggle="dropdown">Courses</a
-              >
-              <ul class="dropdown-menu fade-up">
-                <li>
-                  <a class="dropdown-item" href="course.html">Courses One</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-2.html">Courses Two</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-3.html">Courses Three</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-4.html">Courses Four</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-search.html"
-                    >Courses Search</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-cart.html"
-                    >Courses Cart</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-checkout.html"
-                    >Courses Checkout</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="course-single.html"
-                    >Courses Single</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="."
-                data-bs-toggle="dropdown">Pages</a
-              >
-              <ul class="dropdown-menu fade-up">
-                <li><a class="dropdown-item" href="about.html">About Us</a></li>
-                <li class="dropdown-submenu">
-                  <a class="dropdown-item dropdown-toggle" href=".">Team</a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="team.html">Team</a></li>
-                    <li>
-                      <a class="dropdown-item" href="team-single.html"
-                        >Team Single</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="pricing.html">Pricing Plan</a>
-                </li>
-                <li class="dropdown-submenu">
-                  <a class="dropdown-item dropdown-toggle" href="."
-                    >Instructor</a
-                  >
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="instructor.html"
-                        >Instructor</a
-                      >
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="instructor-profile.html"
-                        >Instructor Profile</a
-                      >
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="become-instructor.html"
-                        >Become Instructor</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li class="dropdown-submenu">
-                  <a class="dropdown-item dropdown-toggle" href=".">Events</a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="event.html">Events</a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="event-single.html"
-                        >Events Single</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="testimonial.html"
-                    >Testimonials</a
-                  >
-                </li>
-                <li class="dropdown-submenu">
-                  <a class="dropdown-item dropdown-toggle" href=".">Account</a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="sign-in.html">Sign In</a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="sign-up.html">Sign Up</a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="profile.html"
-                        >Student Profile</a
-                      >
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="verify-email.html"
-                        >Verify Email</a
-                      >
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="forgot-password.html"
-                        >Forgot Password</a
-                      >
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="reset-password.html"
-                        >Reset Password</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li><a class="dropdown-item" href="faq.html">Faq</a></li>
-                <li><a class="dropdown-item" href="404.html">404 Error</a></li>
-                <li>
-                  <a class="dropdown-item" href="coming-soon.html"
-                    >Coming Soon</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="terms.html">Terms Of Service</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="privacy.html">Privacy Policy</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="."
-                data-bs-toggle="dropdown">Blog</a
-              >
-              <ul class="dropdown-menu fade-up">
-                <li><a class="dropdown-item" href="blog.html">Blog</a></li>
-                <li>
-                  <a class="dropdown-item" href="blog-single.html"
-                    >Blog Single</a
-                  >
-                </li>
-              </ul>
+            <li class="nav-item">
+              <a class="nav-link" href="blog">Blog</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="contactus">Contact</a>
             </li>
           </ul>
           <div class="header-nav-right">
-            <div class="header-nav-search">
-              <a href="." class="search-box-outer"
-                ><i class="far fa-search" /></a
-              >
-            </div>
-            <div class="header-cart">
-              <a href="."><i class="far fa-shopping-cart" /> <span>2</span> </a>
-            </div>
             <div class="header-btn-area">
-              <a href="." class="header-btn-text">Sign In</a>
+              <!-- <a href="." class="header-btn-text">Sign In</a> -->
               <a href="." class="header-btn">Get Started</a>
             </div>
           </div>
