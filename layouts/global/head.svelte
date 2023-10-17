75d5f1cf83ae51def81a3e92d059ed9426329800
jamestagal <benjaminjameswaller@gmail.com>
2023-10-17 21:34:45 +1100

    try cdn for bootstrap

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -23,9 +23,15 @@
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
     crossorigin="anonymous"
   />
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"
+  ></script>
   <link rel="icon" type="image/svg+xml" href="media/logo/favicon.png" />
   <link rel="stylesheet" href="spa/bundle.css" />
   <link rel="stylesheet" href="global.css" />
   <link rel="stylesheet" href="all-fontawesome.min.css" />
   <link rel="stylesheet" href="bootstrap.min.css" />
+  <link rel="stylesheet" href="animate.min.css" />
 </head>
