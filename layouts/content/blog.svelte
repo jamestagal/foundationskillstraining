704c196d2fb81cbd5b5c59715eb0559169c23795
jamestagal <benjaminjameswaller@gmail.com>
2023-10-18 22:43:11 +1100

    create blog page

---
+++ layouts/content/blog.svelte
@@ -0,0 +1,226 @@
+<script>
+  export let title, home, image1, image2, image3, image4;
+</script>
+
+<div class="site-breadcrumb">
+  <div class="hero-shape-area">
+    <img class="hero-shape-1" src={image1.url} alt={image1.alt} />
+    <img class="hero-shape-2" src={image2.url} alt={image2.alt} />
+    <img class="hero-shape-3" src={image3.url} alt={image3.alt} />
+    <img class="hero-shape-4" src={image4.url} alt={image4.alt} />
+  </div>
+  <div class="container">
+    <h2 class="breadcrumb-title">{title}</h2>
+    <ul class="breadcrumb-menu">
+      <li><a href=".">{home}</a></li>
+      <li class="active">{title}</li>
+    </ul>
+  </div>
+</div>
+<!-- blog-area -->
+<div class="blog-area py-120">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/01.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/02.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/03.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/01.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/02.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="blog-item">
+          <div class="blog-item-img">
+            <img src="media/blog/03.jpeg" alt="Thumb" />
+          </div>
+          <div class="blog-item-info">
+            <div class="blog-item-meta">
+              <ul>
+                <li>
+                  <a href="."
+                    ><i class="far fa-user-circle" /> By Alicia Davis</a
+                  >
+                </li>
+                <li>
+                  <a href="."><i class="far fa-calendar-alt" /> May 11, 2023</a>
+                </li>
+              </ul>
+            </div>
+            <h4 class="blog-title">
+              <a href="."
+                >There are many variations of passages orem psum available</a
+              >
+            </h4>
+            <a class="blog-btn" href="."
+              >Read More<i class="far fa-arrow-right" /></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- pagination -->
+    <div class="pagination-area">
+      <div aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="." aria-label="Previous">
+              <span aria-hidden="true"
+                ><i class="far fa-angle-double-left" /></span
+              >
+            </a>
+          </li>
+          <li class="page-item active"><a class="page-link" href=".">1</a></li>
+          <li class="page-item"><a class="page-link" href=".">2</a></li>
+          <li class="page-item"><a class="page-link" href=".">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="." aria-label="Next">
+              <span aria-hidden="true"
+                ><i class="far fa-angle-double-right" /></span
+              >
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- blog-area end -->
