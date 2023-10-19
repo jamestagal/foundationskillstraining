fe7cb1f04bc9fea7f0ca8794802ac4e038b39434
jamestagal <benjaminjameswaller@gmail.com>
2023-10-19 15:03:57 +1100

    add scroll-top

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { onMount } from 'svelte';
-  import Footer from './footer.svelte';
-  import Head from './head.svelte';
-  import Header from './header.svelte';
-  import Login from './login.svelte';
+  import { onMount } from "svelte";
+  import Footer from "./footer.svelte";
+  import Head from "./head.svelte";
+  import Header from "./header.svelte";
+  import Login from "./login.svelte";
 
   export let content, layout, allContent, allLayouts, env, user;
 
@@ -14,16 +14,23 @@
 </script>
 
 <html lang="en">
-<Head title={content?.fields?.meta?.title} {env} {...content.fields} />
-<body id="top">
-  <Header />
-  <main class="main">
-  {#if user && $user.isAuthenticated}
-    <svelte:component this={$user.menu} {user} bind:content={content} />
-  {/if}
-  <Login bind:hash {user} />
-  <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content}/>
- </main>
-<Footer />
-</body>
+  <Head title={content?.fields?.meta?.title} {env} {...content.fields} />
+  <body id="top">
+    <Header />
+    <main class="main">
+      {#if user && $user.isAuthenticated}
+        <svelte:component this={$user.menu} {user} bind:content />
+      {/if}
+      <Login bind:hash {user} />
+      <svelte:component
+        this={layout}
+        {...content.fields}
+        {allContent}
+        {allLayouts}
+        {content}
+      />
+    </main>
+    <Footer />
+    <a href="." id="scroll-top"><i class="far fa-angle-double-up" /></a>
+  </body>
 </html>
